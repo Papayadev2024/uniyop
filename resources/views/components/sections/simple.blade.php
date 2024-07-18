f7be29c3a1a315ef806547d52d9311d4b334a816
Carlos Colina <carlosecolina89@gmail.com>
2024-07-18 08:08:12 -0500

    padin x accesorios

--- resources/views/components/sections/simple.blade.php
+++ resources/views/components/sections/simple.blade.php
@@ -1,4 +1,4 @@
-<section class="w-11/12 mx-auto">
+<section class="w-11/12 mx-auto px-16">
   <h1 class="text-[29px] mx-auto mt-20 font-semibold "> {{ $title }} </h1>
   <div class="flex flex-col md:flex-row gap-5 mt-14 ">
     {{ $slot }}
