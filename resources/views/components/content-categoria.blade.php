5c9615daaf16bdb6c3c71c7da18cb4e1929c03ea
Carlos Colina <carlosecolina89@gmail.com>
2024-07-17 19:26:40 -0500

    fix: categoria

--- resources/views/components/content-categoria.blade.php
+++ resources/views/components/content-categoria.blade.php
@@ -4,11 +4,11 @@
     <h2 class="mt-10 text-[15px] font-normal text-[#333333]"> {{ $item->name }} </h2>
     <h1 class="text-[29px] font-normal"> {{ $item->description }}</h1>
 
-    <button
+    <a href="catalogo/{{ $item->id }}"
       class="bg-[#006BF6] text-[14px] font-semibold text-white text-center p-3 rounded-3xl  items-center justify-center w-32 mb-5 block"
       type="button">
       Comprar ahora
-    </button>
+    </a>
   </div>
   <div class="h-60 relative z-10">
     <img class="block h-80  mx-auto w-[75%] object-cover bottom-0 left-0 -mt-20"
