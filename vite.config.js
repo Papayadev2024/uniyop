b3aebea872701f6c44ffb761bd11d48856bb4687
Carlos Colina <carlosecolina89@gmail.com>
2024-07-18 11:19:07 -0500

    product card jsx

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import glob from 'glob';
-import fs from 'fs';
-import path from 'path';
-
-const components = fs.readdirSync(path.resolve(__dirname, 'resources/js')).filter(file => file.endsWith('.jsx') || file == 'app.js');
-
 
 export default defineConfig({
     server: {
@@ -23,12 +18,6 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        rollupOptions: {
-            // Genera un chunk separado para cada componente
-            input: components.map(component => `resources/js/${component}`),
-        },
-    },
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
         return pages[`./Pages/${name}.jsx`]
