082f0edd7ef168b576b51aa7c64d4d77dc0a3dc7
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-07-19 10:42:53 -0500

    NTS

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -1,5 +1,13 @@
 <x-app-layout>
-  <style>
+  <link href="/js/dxdatagrid/css/dx.light.css?v=06d3ebc8-645c-4d80-a600-c9652743c425" rel="stylesheet" type="text/css"
+    id="dg-default-stylesheet" />
+  <script src="/js/dxdatagrid/js/dx.all.js"></script>
+  <script src="/js/dxdatagrid/js/localization/dx.messages.es.js"></script>
+  <script src="/js/moment/min/moment.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
+  {{-- <style>
     @media (prefers-color-scheme: dark) {
       .dark\:even\:bg-gray-900\/50:nth-child(even) {
         background-color: transparent !important;
@@ -7,7 +15,7 @@
         border-bottom: 1px solid rgb(55 65 81 / 0.25);
       }
     }
-  </style>
+  </style> --}}
   <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
     <section class="py-4 border-b border-slate-100 dark:border-slate-700">
@@ -29,18 +37,14 @@
 
         <!-- Table -->
         <div class="overflow-x-auto">
-
+          {{-- 
           <table id="tabladatos" class="display text-lg" style="width:100%">
             <thead>
               <tr>
                 <th>Producto</th>
-                {{-- <th>Extracto</th>
-                <th>Descripcion</th> --}}
                 <th>Precio</th>
                 <th>Descuento</th>
-                {{-- <th>Costo por articulo</th> --}}
                 <th>Stock</th>
-                {{-- <th>Peso</th> --}}
                 <th>Imagen</th>
                 <th>Destacar</th>
                 <th>Recomendar</th>
@@ -56,13 +60,9 @@
                       - {{ $item->color }}
                     @endif
                   </td>
-                  {{-- <td>{{ $item->extract }}</td>
-                  <td>{{ $item->description }}</td> --}}
                   <td>{{ $item->precio }}</td>
                   <td>{{ $item->descuento }}</td>
-                  {{-- <td>{{ $item->costo_x_art }}</td> --}}
                   <td>{{ $item->stock }}</td>
-                  {{-- <td>{{ $item->peso }}</td> --}}
                   <td class="px-3 py-2"><img class="bg-[#f2f2f2] w-20 h-20 object-cover object-center"
                       src="{{ asset($item->imagen) }}" alt=""></td>
                   <td>
@@ -143,13 +143,9 @@
             <tfoot>
               <tr>
                 <th>Producto</th>
-                {{-- <th>Extracto</th>
-                <th>Descripcion</th> --}}
                 <th>Precio</th>
                 <th>Descuento</th>
-                {{-- <th>Costo por articulo</th> --}}
                 <th>Stock</th>
-                {{-- <th>Peso</th> --}}
                 <th>Imagen</th>
                 <th>Destacar</th>
                 <th>Recomendar</th>
@@ -157,7 +153,9 @@
                 <th>Acciones</th>
               </tr>
             </tfoot>
-          </table>
+          </table> --}}
+
+          <div id="gridContainer"></div>
 
         </div>
       </div>
@@ -168,6 +166,202 @@
 
 
 </x-app-layout>
+
+<script>
+  const salesDataGrid = $('#gridContainer').dxDataGrid({
+    language: "es",
+    dataSource: {
+      load: async (params) => {
+        const res = await fetch("{{ route('products.paginate') }}", {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'XSRF-TOKEN': decodeURIComponent(Cookies.get('XSRF-TOKEN'))
+          },
+          body: JSON.stringify({
+            _token: $('[name="_token"]').val(),
+            ...params
+          })
+        })
+        const data = await res.json()
+        return data
+      },
+    },
+    onToolbarPreparing: (e) => {
+      const items = e.toolbarOptions.items;
+      items.unshift({
+        widget: 'dxButton',
+        location: 'after',
+        options: {
+          icon: 'revert',
+          hint: 'REFRESCAR TABLA',
+          onClick: () => {
+            salesDataGrid.refresh()
+          }
+        }
+      });
+    },
+    remoteOperations: true,
+    columnResizingMode: "widget",
+    allowColumnResizing: true,
+    allowColumnReordering: true,
+    columnAutoWidth: true,
+    scrollbars: 'auto',
+    // filterPanel: {
+    //   visible: true
+    // },
+    searchPanel: {
+      visible: true
+    },
+    // headerFilter: {
+    //   visible: true,
+    //   search: {
+    //     enabled: true
+    //   }
+    // },
+    // height: 'calc(100vh - 185px)',
+    rowAlternationEnabled: true,
+    showBorders: true,
+    paging: {
+      pageSize: 5,
+    },
+    pager: {
+      visible: true,
+      allowedPageSizes: [5, 10, 25, 50, 100],
+      showPageSizeSelector: true,
+      showInfo: true,
+      showNavigationButtons: true,
+    },
+    allowFiltering: true,
+    scrolling: {
+      mode: 'standard',
+      useNative: true,
+      preloadEnabled: true,
+      rowRenderingMode: 'standard'
+    },
+    columns: [{
+        dataField: 'producto',
+        caption: 'PRODUCTO',
+        width: '40%'
+      },
+      {
+        dataField: 'precio',
+        caption: 'PRECIO'
+      },
+      {
+        dataField: 'descuento',
+        caption: 'DESCUENTO'
+      },
+      {
+        dataField: 'stock',
+        caption: 'STOCK'
+      },
+      {
+        caption: 'ESTADO',
+        cellTemplate: (container, {
+          data
+        }) => {
+          container.addClass('!px-3 !py-2 !text-center')
+          container.css('vertical-align', 'middle')
+          container.html(
+            `<img class="bg-[#f2f2f2] w-16 h-10 object-cover object-center rounded-sm" src="/${data.imagen}" alt=""></td>`
+          )
+        }
+      },
+      {
+        dataField: 'destacar',
+        caption: 'DESTACAR',
+        cellTemplate: (container, {
+          data
+        }) => {
+          container.html(`<form method="POST" action="">
+            @csrf
+            <input type="checkbox" id="hs-basic-usage"
+              class="check_v btn_swithc relative w-[3.25rem] h-7 p-px bg-gray-100 border-transparent text-transparent 
+                    rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-transparent disabled:opacity-50 disabled:pointer-events-none 
+                    checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-gray-800 dark:border-gray-700 
+                    dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:size-6
+                    before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow 
+                    before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-gray-400 dark:checked:before:bg-blue-200"
+              id='d_${data.id}' data-field='destacar' data-idService='${data.id}'
+              data-titleService='${data.producto}' ${data.destacar ? 'checked': ''}>
+            <label for="d_${data.id}"></label>
+          </form>`)
+        }
+      },
+      {
+        dataField: 'recomendar',
+        caption: 'RECOMENDAR',
+        cellTemplate: (container, {
+          data
+        }) => {
+          container.html(`<form method="POST" action="">
+            @csrf
+            <input type="checkbox" id="hs-basic-usage"
+              class="check_v btn_swithc relative w-[3.25rem] h-7 p-px bg-gray-100 border-transparent text-transparent 
+                    rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-transparent disabled:opacity-50 disabled:pointer-events-none 
+                    checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-gray-800 dark:border-gray-700 
+                    dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:size-6
+                    before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow 
+                    before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-gray-400 dark:checked:before:bg-blue-200"
+              id='r_${data.id}' data-field='recomendar' data-idService='${data.id}'
+              data-titleService='${data.producto}' ${data.recomendar ? 'checked': ''}>
+            <label for="r_${data.id}"></label>
+          </form>`)
+        }
+      },
+      {
+        dataField: 'visible',
+        caption: 'VISIBLE',
+        cellTemplate: (container, {
+          data
+        }) => {
+          container.html(`<form method="POST" action="">
+            @csrf
+            <input type="checkbox" id="switch_visible"
+              class="check_v btn_swithc relative w-[3.25rem] h-7 p-px bg-gray-100 border-transparent text-transparent 
+                    rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:ring-transparent disabled:opacity-50 disabled:pointer-events-none 
+                    checked:bg-none checked:text-blue-600 checked:border-blue-600 focus:checked:border-blue-600 dark:bg-gray-800 dark:border-gray-700 
+                    dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-600 before:inline-block before:size-6
+                    before:bg-white checked:before:bg-blue-200 before:translate-x-0 checked:before:translate-x-full before:rounded-full before:shadow 
+                    before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-gray-400 dark:checked:before:bg-blue-200"
+              id='v_${data.id}' data-field='visible' data-idService='${data.id}'
+              data-titleService='${data.producto}' ${data.visible ? 'checked' : ''}>
+            <label for="v_${data.id}"></label>
+          </form>`)
+        }
+      },
+      {
+        caption: 'ACCIONES',
+        cellTemplate: (container, {
+          data
+        }) => {
+          container.addClass('!px-3 !py-2 !text-center')
+          container.css('vertical-align', 'middle')
+          container.html(`<a href="/admin/products/${data.id}/edit"
+            class="inline-block bg-yellow-400 px-3 py-2 rounded text-white  ">
+            <i class="fa-regular fa-pen-to-square"></i>
+          </a>
+          <form action="" method="POST" class="inline-block">
+            @csrf
+            <a data-idService='${data.id}'
+              class="btn_delete bg-red-600 px-3 py-2 rounded text-white cursor-pointer">
+              <i class="fa-regular fa-trash-can"></i>
+            </a>
+          </form>`)
+        }
+      }
+    ],
+    onContentReady: (...props) => {
+      tippy('.tippy-here', {
+        arrow: true,
+        animation: 'scale'
+      })
+    }
+  }).dxDataGrid('instance')
+</script>
+
 <script>
   $('document').ready(function() {
 
