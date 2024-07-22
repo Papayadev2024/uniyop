ecce841e0bd828ed2059813855c496366e4c4beb
Carlos Colina <carlosecolina89@gmail.com>
2024-07-22 08:38:56 -0500

    truncar texto

--- app/Http/Controllers/NewsletterSubscriberController.php
+++ app/Http/Controllers/NewsletterSubscriberController.php
@@ -117,7 +117,7 @@ class NewsletterSubscriberController extends Controller
             <table
               style="
                 width: 600px;
-                height: 800px;
+                height: 850px;
                 margin: 0 auto;
                 text-align: center;
                 background-image:url(' . $appUrl . 'images/Ellipse_18.png),  url(' . $appUrl . 'images/Tabpanel.png);
