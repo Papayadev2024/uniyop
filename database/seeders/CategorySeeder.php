4049bdb10f83f5867c708d267886a10c7b9bae02
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-07-24 16:21:58 -0500

    Subiendo el seeder de imagenes

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -7,25 +7,39 @@ use Illuminate\Database\Seeder;
 
 use App\Models\Category;
 
+use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+
 class CategorySeeder extends Seeder
 {
+    use Importable;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
-        $cat = ['Accesorios de Oficina','Accesorios para Auto','Accesorios Gamer','Accesorios para celular','Accesorios para Laptop','Accesorios para Ipad / tablets','Accesorios Audio y Video','Accesorios Home','Accesorios de Reloj','Packs'];
+        Excel::import(
+            new class implements ToModel
+            {
+                public function model(array $row)
+                {
+                    if (!is_numeric($row[0])) return null;
 
-        foreach ($cat as $categoryName) {
-            Category::updateOrCreate([
-                'name' => $categoryName,
-            ],[
-                'name' => $categoryName,
-                'description' => 'Aquí va la descripción de la categoria ' . $categoryName,
-                'status' => 1,
-                'visible' => 1,
-            ]);
-        }
+                    Category::updateOrCreate([
+                        'id' => $row[0]
+                    ], [
+                        'id' => $row[0],
+                        'name' => $row[1],
+                        'slug' => str_replace(' ', '-', strtolower($row[1])),
+                        'url_image' => 'images/img/',
+                        'name_image' => 'noimagen.jpg',
+                        'destacar' => false,
+                        'fit' => 'contain'
+                    ]);
+                }
+            },
+            'storage/app/utils/Categories.xlsx'
+        );
     }
 }
--- database/seeders/SubcategorySeeder.php
+++ database/seeders/SubcategorySeeder.php
@@ -2,30 +2,47 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use App\Models\SubCategory;
 
+use Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+
 class SubcategorySeeder extends Seeder
 {
+    use Importable;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
-        $subcat = ['Audio y Video','Auriculares y Audifonos','Cables','Camaras','Parlantes','Cargadores Multifuncion','Mochilas','Reloj','Cargadores','Protector de reloj','teclados y mouse','camaras web','audifonos gamer','auriculares gamer','pad mouse gamer','microfonos gamer','sillas gamer','Home Escritorio','Home Cocina','Home Baño','Home Deco','Cargadores','Soportes','Otros accesorios de Auto','Audio y Video','Cables','Cargadores de Celular','Cargadores Multifuncion','Cases de Celular','Powerbank','Protector de Celular','Soportes de celular','Otros accesorios de celular','Cases Tablet','Protector Tablet','Soportes Tablet','Otro accesorios de tablet','Audio y Video','Cables','Case de Disco','Case de Laptop','Hubs','LamparaMouse','Mouse Pad','Protector de Laptop','Soportes de Laptop','Soportes y Coolers','Teclados','Otros accesorios de laptop','Accesorios para Ipad / tablets','Cases Tablet','Protector Tablet','Soportes Tablet','Otro accesorios de tablet','Pack dia de la Madre','Pack dia del Padres','Packs Navideños','Otros Packs'];
-        $id_categoria = ['7','7','7','7','7','1','1','9','9','9','3','3','3','3','3','3','3','8','8','8','8','2','2','2','4','4','4','4','4','4','4','4','4','6','6','6','6','5','5','5','5','5','5','5','5','5','5','5','5','5','5','5','5','5','10','10','10','10'];
+        Excel::import(new class implements ToModel
+        {
+            public function model(array $row)
+            {
+                if (!is_numeric($row[0])) return null;
 
-        foreach ($subcat as $key =>$categoryName) {
-            SubCategory::create([
-                'name' => $subcat[$key],
-                'category_id' => $id_categoria[$key],
-                'description' => 'Aquí va la descripción de la categoria ' . $subcat[$key],
-                'status' => 1,
-                'visible' => 1,
-            ]);
-        }
+                $category = Category::updateOrCreate(['id' => $row[1]], [
+                    'id' => $row[1],
+                    'name' => $row[2],
+                    'slug' => str_replace(' ', '-', strtolower($row[2])),
+                    'url_image' => 'images/img/',
+                    'name_image' => 'noimagen.jpg'
+                ]);
+
+                SubCategory::updateOrCreate(['id' => $row[0]], [
+                    'id' => $row[0],
+                    'category_id' => $category->id,
+                    'name' => $row[3],
+                    'slug' => str_replace(' ', '-', strtolower($row[3])),
+                    'status' => true,
+                    'visible' => true
+                ]);
+            }
+        }, 'storage/app/utils/SubCategories.xlsx');
     }
 }
